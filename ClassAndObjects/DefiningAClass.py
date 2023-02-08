639cb7629ad1caec1e20a15309dcfd941a081c90
Varun <bhat.varun.25@gmail.com>
2023-02-08 12:04:02 +0530

    LearnPython

---
+++ ClassAndObjects/DefiningAClass.py
@@ -0,0 +1,17 @@
+'''
+Defining a class
+to print "Hello World!!"
+'''
+
+class PrintHelloWorld:
+    # Functions inside class to replicate the behavior of object.
+
+    def print_hello_world(self):
+        print("Hello World!!")
+
+
+# Creating the objects for class "PrintHelloWorld"
+obj = PrintHelloWorld()
+
+# Calling the object obj to pint "Hello World!!"
+PrintHelloWorld.print_hello_world(obj)
